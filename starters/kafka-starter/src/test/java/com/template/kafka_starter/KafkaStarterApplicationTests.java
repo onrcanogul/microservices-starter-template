f58fc06f95d36b3b7aed891eb92972a98082982a
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-24 14:17:00 +0300

    messaging module was created

---
+++ starters/kafka-starter/src/test/java/com/template/kafka_starter/KafkaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.kafka_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
